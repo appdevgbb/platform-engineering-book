300826ea2d60fdff2b0c975634cb509f68680129
Diego Casati <diego.casati@microsoft.com>
2023-10-24 16:08:50 +0000

    update index. initial clean-up of the template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Platform Engineering Cookbook'
+copyright = '2021, Azure Global Black Belt Team'
+author = 'GBB'
 
 release = '0.1'
 version = '0.1.0'
